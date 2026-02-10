bd7702299689a051f04f31a8ccab125e9fff338a
JSeHoone <beebea545@gmail.com>
2026-02-10 22:46:07 +0900

    feat[user] : 일별 사용자 가입 수 집계 쿼리 추가
    - 일별 신규 가입자 수 집계를 위해 퀴리를 추가하였습니다.

--- src/main/java/com/shortudy/backoffice/domain/user/repository/UserRepository.java
+++ src/main/java/com/shortudy/backoffice/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.shortudy.backoffice.domain.user.repository;
 
+import com.shortudy.backoffice.domain.dashboard.dto.projection.DailyUserCount;
 import com.shortudy.backoffice.domain.user.dto.response.UserProjection;
 import com.shortudy.backoffice.domain.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,10 +9,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+    /*
+    * [일별 사용자 가입 수 집계]
+    * */
+    @Query("SELECT FUNCTION('date', u.createdAt) as date, COUNT(u) as count " +
+            "FROM User u " +
+            "WHERE u.createdAt >= :from AND u.createdAt < :to " +
+            "GROUP BY FUNCTION('date', u.createdAt)")
+    List<DailyUserCount> countDailyUsers(@Param("from") LocalDateTime from,
+                                         @Param("to") LocalDateTime to);
 
     @Query("SELECT u.email as email, u.nickname as nickname, u.role as role, " +
             "u.status as status, r.updatedAt as updatedAt " +
