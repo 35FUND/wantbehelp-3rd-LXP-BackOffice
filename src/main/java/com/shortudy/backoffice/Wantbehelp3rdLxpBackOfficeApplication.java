2b2f4e5a15250846c3d062bb4fcb5a29fe26c9f7
JSeHoone <beebea545@gmail.com>
2026-02-10 11:29:40 +0900

    first commit

---
+++ src/main/java/com/shortudy/backoffice/Wantbehelp3rdLxpBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.shortudy.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wantbehelp3rdLxpBackOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wantbehelp3rdLxpBackOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shortudy/backoffice/Wantbehelp3rdLxpBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortudy.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wantbehelp3rdLxpBackOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
