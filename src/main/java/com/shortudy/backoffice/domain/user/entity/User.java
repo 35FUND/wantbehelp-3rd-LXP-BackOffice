a858d32ccc25e25f012c10eb2d7aecd679834ed0
JinYong833 <jjystudy1@gmail.com>
2026-02-11 17:39:36 +0900

    fix[user] : user테이블에 lastLoginAt nullable하게 변경

--- src/main/java/com/shortudy/backoffice/domain/user/entity/User.java
+++ src/main/java/com/shortudy/backoffice/domain/user/entity/User.java
@@ -30,7 +30,6 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private UserRole role;
 
-    // 전체 url이 아닌 profile image key 값만 저장
     @Column
     private String profileUrl;
 
@@ -46,8 +45,6 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
-    @CreatedDate
-    @Column(nullable = false)
     private LocalDateTime lastLoginAt;
 
     protected User() {}
